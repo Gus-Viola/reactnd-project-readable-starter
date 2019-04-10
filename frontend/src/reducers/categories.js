9df00730308b6b9f6e42d3049020334291466b5e
Gus Viola <gus.viola@pobox.com>
2019-04-10 11:36:36 -0300

    Start reducers implementation

---
+++ frontend/src/reducers/categories.js
@@ -0,0 +1,12 @@
+import { GET_CATEGORIES } from '../actions/categories';
+
+export default function categories(state = [], action) {
+  const { data } = action;
+
+  switch (action.type) {
+    case GET_CATEGORIES:
+      return data;
+    default:
+      return state;
+  }
+};
---
+++ frontend/src/reducers/posts.js
@@ -0,0 +1,27 @@
+import { GET_TOTAL_POSTS, GET_POSTS_BY_CATEGORY, GET_POST, ADD_POST,
+  EDIT_POST, DELETE_POST, UPVOTE_POST } from '../actions/posts';
+
+export default function posts(state = {}, action) {
+  const { data } = action;
+
+  switch (action.type) {
+    case GET_TOTAL_POSTS:
+      return _.mapKeys(data, 'id'); //is this right?
+      // return { ...state, [data.id]: data };
+
+    case GET_POSTS_BY_CATEGORY:
+      return { ...state, [data.id]: data };
+    case GET_POST :
+      return { ...state, [data.id]: data };
+    case ADD_POST:
+      return { ...state, [data.id]: data };
+    case EDIT_POST:
+      return { ...state, [data.id]: data };
+    case DELETE_POST :
+      return { ...state, [data.id]: data };
+    case UPVOTE_POST :
+      return { ...state, [data.id]: data };
+    default:
+      return state;
+    }
+  };
